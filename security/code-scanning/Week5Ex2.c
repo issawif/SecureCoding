c18dd4664afb47b026dcea86d3191a15ed26cd81
Tums <issawif@users.noreply.github.com>
2024-03-08 16:45:16 +0300

    Update Week5Ex2.c

--- security/code-scanning/Week5Ex2.c
+++ security/code-scanning/Week5Ex2.c
@@ -4,7 +4,7 @@
 int main(int argc, char** argv) {
   char *filename = argv[1];
   
-/*
+
 
   FILE* ptr = fopen(filename, "r");
   
@@ -12,7 +12,7 @@ int main(int argc, char** argv) {
   printf("File cannot be opened.\n");
   return(-1);
   }
-  
+/*  
   char buff[BUFFSIZE];
   
   while (fgets(buff, BUFFSIZE, ptr) != NULL) {
