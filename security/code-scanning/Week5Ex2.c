a51f1341ea16943c82a169561fa00d7161acd825
Tums <issawif@users.noreply.github.com>
2024-03-08 16:35:22 +0300

    Update Week5Ex2.c

--- security/code-scanning/Week5Ex2.c
+++ security/code-scanning/Week5Ex2.c
@@ -2,23 +2,23 @@
 #include <stdlib.h>
 
 int main(int argc, char** argv) {
-  const char *filename = argv[1];
+  char *filename = argv[1];
   
 
 
-FILE* ptr = fopen(filename, "r");
-
-if (ptr == NULL) {
-printf("File cannot be opened.\n");
-return(-1);
-}
-
-char buff[BUFFSIZE];
-
-while (fgets(buff, BUFFSIZE, ptr) != NULL) {
-printf("%s", buff);
-}
-
-return 0; 
+  FILE* ptr = fopen(filename, "r");
+  
+  if (ptr == NULL) {
+  printf("File cannot be opened.\n");
+  return(-1);
+  }
+  
+  char buff[BUFFSIZE];
+  
+  while (fgets(buff, BUFFSIZE, ptr) != NULL) {
+  printf("%s", buff);
+  }
+  
+  return 0; 
 }
 
