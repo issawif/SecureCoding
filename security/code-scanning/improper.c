5458d8a274ac67ff35e5c1ec43c6fdcd39b3a443
Tums <issawif@users.noreply.github.com>
2024-03-08 11:03:45 +0300

    Update improper.c

--- security/code-scanning/improper.c
+++ security/code-scanning/improper.c
@@ -6,10 +6,8 @@ int main(int argc, char** argv) {
   char *userName = argv[1];
   
   char command1[1000] = {0};
-  char userNameEncoded[1000] = {0};
   
-  encodeShellString(userNameEncoded, 1000, userName);  
-  snprintf(command1, 1000, "ls %s", userNameEncoded);
+  snprintf(command1, 1000, "ls %s", userName);
   system(command1);
    
 }
