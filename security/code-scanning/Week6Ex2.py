f93823a8c30c65ca8914516ae7135bb1257cb6cd
Tums <issawif@users.noreply.github.com>
2024-03-11 23:10:16 +0300

    Create Week6Ex2.py

---
+++ security/code-scanning/Week6Ex2.py
@@ -0,0 +1,11 @@
+def get_value_from_array(array, index):
+# Retrieve the element from the array at the specified index
+If index > 0 and index < len(array):
+		Return array[index]
+	Return None
+
+array = [10, 20, 30, 40, 50]
+
+# Test the get_value_from_array function
+print("Value at index 2:", get_value_from_array(array, 2))  # Should print 30
+print("Value at index 5:", get_value_from_array(array, 5))  # Should print an error message and return None
